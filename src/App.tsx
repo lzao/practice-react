9f21817165d9a2ed736d7a3984a49491ae6b2281
김준희 <lzqa@naver.com>
2021-10-29 16:43:34 +0900

    - bootstrap 를 설치하여 코로나 확진자 수를 bootstrap 테이블로 노출합니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import './App.css';
-import React from 'react';
+import React, {ReactElement} from 'react';
 import useDailyCorona from './hooks/useDailyCorona';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table} from 'react-bootstrap';
 
-function App() {
+export default function App(): ReactElement {
   const items = useDailyCorona();
 
   return (
-    <ul>
-      {items?.map((item, index) => (
-        <li key={index}>
-          <span>조회 시작 시간: {item.createDt}</span>
-          <span>조회 수정 시간: {item.updateDt}</span>
-          <span>사망자 수: {item.deathCnt}</span>
-        </li>
-      ))}
-    </ul>
+    <Table className="table table-striped table-dark">
+      <thead className="thread-dark">
+        <tr>
+          <th scope="col">지역</th>
+          <th scope="col">확진자수</th>
+          <th scope="col">전일대비</th>
+          <th scope="col">격리해제수</th>
+          <th scope="col">사망자수</th>
+          <th scope="col">해외유입수</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items?.map((item, index) => (
+          <tr key={index}>
+            <th scope="row">{item.gubun}</th>
+            <th scope="row">{item.defCnt.toLocaleString()}</th>
+            <th scope="row">{item.incDec.toLocaleString()}</th>
+            <th scope="row">{item.isolClearCnt.toLocaleString()}</th>
+            <th scope="row">{item.deathCnt.toLocaleString()}</th>
+            <th scope="row">{item.overFlowCnt.toLocaleString()}</th>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
-
-export default App;
