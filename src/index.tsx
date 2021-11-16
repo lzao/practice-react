e8e9faf2a5cceaf192c2d14ace56bb56f7be754b
june <lzao@naver.com>
2021-11-17 02:46:02 +0900

    - msw 를 적용하여 mocking 을 작성합니다

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const {worker} = require('./mocks/browser');
+
+  worker.start();
+}
 ReactDOM.render(<App />, document.getElementById('root'));
