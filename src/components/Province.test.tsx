c073501b491b9d63c430a7b1ff21f90b082814a8
김준희 <lzqa@naver.com>
2021-11-17 10:54:29 +0900

    - 일일 코로나 현황 테이블에 css 를 적용합니다 - 일일 코로나 현황 데이터가 없을 경우 'empty.' 텍스트가 포함된 컬럼을 보여줍니다.

--- src/components/Province.test.tsx
+++ src/components/Province.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import Province from './Province';
+import handlers from '../mocks/handlers';
+import {setupServer} from 'msw/node';
 
 describe('Province', () => {
+  const server = setupServer(...handlers);
+  // 테스트 시작 전에 서버를 실행한다.
+  beforeAll(() => server.listen());
+
+  // 테스트가 끝날 때마다 리퀘스트 핸들러를 초기화하여 서로 영향을 미치지 않도록 한다.
+  afterEach(() => server.resetHandlers());
+
+  // 테스트가 완료되면 서버를 종료한다.
+  afterAll(() => server.close());
   it('aaaa', () => {
-    console.log('abc');
+    server.use();
   });
 });
