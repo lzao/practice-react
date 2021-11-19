c405598039dc4bf9e69cc6e5c3502e0cbc7e2823
김준희 <lzqa@naver.com>
2021-11-19 13:58:29 +0900

    useDailyCorona 가 두번 호출되는 이슈를 수정합니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, {ReactElement} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Province from './components/Province';
 
 export default function App(): ReactElement {
-  return <Province></Province>;
+  return <Province />;
 }
--- src/hooks/useDailyCorona.tsx
+++ src/hooks/useDailyCorona.tsx
@@ -1,16 +1,49 @@
 import {useEffect, useState} from 'react';
-import {getCorona} from '../api/corona';
 import coronaApiProps from '../interfaces/coronaApiProps.interface';
+import axios from "axios";
+import moment from "moment";
+import {DAILY_CORONA_STATUS_API_SERVICE_KEY} from "../constants";
 
 export default function useDailyCorona(): coronaApiProps[] | undefined {
   const [items, setItems] = useState<coronaApiProps[]>();
 
-  const coronaItems = getCorona();
+  const params = setParams();
+
+  async function setCorona() {
+    await axios
+      .get('/openapi/service/rest/Covid19/getCovid19SidoInfStateJson', {
+        params,
+      })
+      .then(response => {
+        setItems(JSON.parse(JSON.stringify(response.data)).response.body.items.item);
+      });
+  }
+
   useEffect(() => {
-    coronaItems.then(response => {
-      setItems(response);
-    });
+    let isLoading = true;
+    if (isLoading) {
+      setCorona();
+    }
+    return () => {
+      isLoading = false;
+    }
   }, []);
 
   return items;
+}
+
+function setParams() {
+  const date = getNowDate();
+  const params = {
+    ServiceKey: DAILY_CORONA_STATUS_API_SERVICE_KEY,
+    pageNo: 1,
+    numOfRows: 20,
+    startCreateDt: date,
+    endCreateDt: date,
+  };
+  return params;
+}
+
+function getNowDate(): string {
+  return moment().format('YYYYMMDD');
 }
