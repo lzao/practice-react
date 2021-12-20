52a4534b556045d4f3f2b7a6e4be46947212d21e
김준희 <lzqa@naver.com>
2021-12-20 15:35:46 +0900

    실시간 코로나 확진자 전광판의 애니메이션과 스타일을 추가합니다

--- src/components/RealTimeConfirmedCase.tsx
+++ src/components/RealTimeConfirmedCase.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, {ReactElement, useEffect, useRef, useState} from 'react';
+import styled, { keyframes } from 'styled-components';
 
 interface realTimeConfirmedCase {
   id: number
@@ -7,6 +8,53 @@ interface realTimeConfirmedCase {
   province: string
   confirmedCaseCount: number
 }
+
+const BoxFadeIn = keyframes`
+  from {
+      transform: translate3d(0,10px,0)
+  }
+  to {
+      transform: translate3d(0,0,0);
+      opacity: 1
+  }
+`;
+
+const Container = styled.ul`
+  border: 1px solid #e6e6ea;
+  box-shadow: rgb(0 0 0 / 4%) 0px 2px 10px 0px;
+  border-radius: 0.725em;
+  padding: 1em;
+  margin-bottom: 3em;
+  background: linear-gradient(180deg, #f7f7f7, transparent);
+  font-size: 0.75em;
+` 
+
+const ListContainer = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  animation: 350ms ease 0s 1 ${BoxFadeIn};
+`;
+
+const TimeContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const BellIcon = styled.svg`
+  width: 1em;
+  height: 1em;
+`;
+
+const TimeText = styled.div`
+  padding-left: 0.5em;
+`;
+
+const MoreNotifyButton = styled.svg`
+  width: 1.3em;
+  height: 1.3em;
+`;
+
 
 export default function RealTimeConfirmedCase(): ReactElement {
   const isCalled = useRef(false);
@@ -28,18 +76,30 @@ export default function RealTimeConfirmedCase(): ReactElement {
   }, []);
 
   return (
-    <div>
+    <Container>
       {!items ? (
         <div>확진된 사람이 없습니다.</div>
       ) : (
         items.map((item) => (
-          <li key={item.id} role="slick-realtime">
-            <p>시간: {item.time}</p>
-            <p>지역: {item.province}</p>
-            <p>확진자수: {item.confirmedCaseCount}</p>
-          </li>
+          <ListContainer key={item.id} role="slick-realtime">
+            <TimeContainer>
+              <BellIcon viewBox="0 0 24 24">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M20 17h2v2H2v-2h2v-7a8 8 0 1 1 16 0v7zm-2 0v-7a6 6 0 1 0-12 0v7h12zm-9 4h6v2H9v-2z" />
+              </BellIcon>
+              <TimeText>{item.time}</TimeText>
+            </TimeContainer>
+            <div>
+              <strong>{item.province}</strong> {item.confirmedCaseCount}명 추가 확진
+            </div>
+            <div>
+            <MoreNotifyButton viewBox="0 0 250 250">
+              <path d="M38.399,76.8c1.637,0,3.274,0.625,4.524,1.875l85.075,85.076l85.075-85.076c2.5-2.5,6.55-2.5,9.05,0s2.5,6.55,0,9.05  l-89.6,89.601c-2.5,2.5-6.551,2.5-9.051,0l-89.6-89.601c-2.5-2.5-2.5-6.55,0-9.05C35.124,77.425,36.762,76.8,38.399,76.8z" />
+            </MoreNotifyButton>
+            </div>
+          </ListContainer>
         ))
       )}
-    </div>
+    </Container>
   );
 }
