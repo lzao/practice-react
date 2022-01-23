c9ad274053756a4566eed770fbe568484c4f08b1
june <lzao@naver.com>
2022-01-24 02:34:04 +0900

    Province Component 의 첫번째 테스트 케이스를 수정합니다

--- src/components/Province.test.tsx
+++ src/components/Province.test.tsx
@@ -4,19 +4,15 @@ import Province from './Province';
 import {taskErrorHandler} from '../mocks/handlers';
 import server from '../setupTests';
 
-test('일일 코로나 현황 검역의 확진자수가 6363 입니다.', async () => {
+test('일일 코로나 현황 검역의 확진자수가 6363명 입니다.', async () => {
   render(<Province />);
-  const displayed = await waitFor(() => screen.getByText('6,363'), {
-    timeout: 500,
-  });
+  const displayed = await waitFor(() => screen.getByText('6,363명'));
   expect(displayed).toBeInTheDocument();
 });
 
 test('일일 코로나 현황의 데이터가 없으면 empty. 를 노출합니다.', async () => {
   server.use(taskErrorHandler);
   render(<Province />);
-  const displayed = await waitFor(() => screen.getByText('empty.'), {
-    timeout: 500,
-  });
+  const displayed = await waitFor(() => screen.getByText('empty.'));
   expect(displayed).toBeInTheDocument();
 });
