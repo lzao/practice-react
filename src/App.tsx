d8bb02804a9a4535cf28665cae60703addd5082f
김준희 <lzqa@naver.com>
2022-06-21 10:16:55 +0900

    모달을 생성합니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import Modal from 'components/Modal';
 import RealTimeConfirmedCase from 'components/RealTimeConfirmedCase';
-import React, {ReactElement} from 'react';
+import modalHeader from 'interfaces/modalHeader.interface';
+import modalItems from 'interfaces/modalItems.interface';
+import React, {ReactElement, useState} from 'react';
 import Province from './components/Province';
+import './css/common.css';
 
 export default function App(): ReactElement {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [header, setHeader] = useState<modalHeader>({});
+  const [modalItem, setModalItems] = useState<modalItems[]>([]);
+
+  const openModal = () => {
+    setModalOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    document.body.style.overflow = 'unset';
+  };
+
   return (
     <>
-      <RealTimeConfirmedCase />
+      <Modal open={modalOpen} close={closeModal} header={header} items={modalItem} />
+      <RealTimeConfirmedCase openModal={openModal} setHeader={setHeader} setModalItems={setModalItems} />
       <Province />
     </>
   );
---
+++ src/components/Modal.tsx
@@ -0,0 +1,74 @@
+import modalHeader from 'interfaces/modalHeader.interface';
+import modalItems from 'interfaces/modalItems.interface';
+import {DAILY_CONFIREMD_CITY} from '../constants';
+import React, {ReactElement} from 'react';
+import styled from 'styled-components';
+
+type props = {
+  open: boolean,
+  close: () => void,
+  header: modalHeader,
+  items: modalItems[],
+};
+
+const Container = styled.div`
+  &.openModal {
+    position: fixed;
+    height: 100%;
+    background-color: #fff;
+    width: 100%;
+    z-index: 1;
+    padding: 1em;
+    box-sizing: border-box;
+  }
+`;
+
+export default function Modal(props: props): ReactElement {
+  const {open, close, header, items} = props;
+
+  const headerKeys = Object.keys(header);
+
+  const city = DAILY_CONFIREMD_CITY;
+
+  return (
+    <Container className={open ? 'openModal modal' : 'modal'}>
+      {open ? (
+        <section>
+          <div>실시간 확진자 현황</div>
+          <header>
+            {!header ? (
+              <div>리스트가 없습니다.</div>
+            ) : (
+              headerKeys.map((item, index) => {
+                return (
+                  <span key={index}>
+                    {item}: {header[item]}
+                  </span>
+                );
+              })
+            )}
+          </header>
+          <main>
+            {!items ? (
+              <div>리스트가 없습니다.</div>
+            ) : (
+              items.map((item, index) => {
+                return (
+                  <div key={index}>
+                    {city[item.cityId].name} {city[item.cityId][item.guId] ? `${city[item.cityId][item.guId]} ` : ''}
+                    {item.cases.toLocaleString()}명 추가 확진
+                  </div>
+                );
+              })
+            )}
+          </main>
+          <footer>
+            <button className="close" onClick={close}>
+              close
+            </button>
+          </footer>
+        </section>
+      ) : null}
+    </Container>
+  );
+}
